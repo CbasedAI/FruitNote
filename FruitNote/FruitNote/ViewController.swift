77fe64f812c7525c6df84bd7fff39cc9d7490b86
artinhuang <artinhuangcn@gmail.com>
2018-12-23 02:09:59 +0800

    init

---
+++ FruitNote/FruitNote/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FruitNote
+//
+//  Created by artin on 2018/12/23.
+//  Copyright © 2018年 artinhuang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
